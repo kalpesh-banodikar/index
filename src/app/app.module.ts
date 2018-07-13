e8e7f23e1a6897f51aaf1c7db638c6be86be9931
MSR-K <38326787+MSR-K@users.noreply.github.com>
2018-07-13 15:24:56 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { RouterModule } from '@angular/router' ;
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomepageComponent,
     LandingPageComponent,
-    FooterComponent
+    FooterComponent,
+    LoginSignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path:'', component:LandingPageComponent},
+      {path:'login' , component:LoginSignupComponent},
+      { path:'lp', component:LandingPageComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
